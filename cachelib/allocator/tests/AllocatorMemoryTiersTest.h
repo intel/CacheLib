777ffbdbf1bef0425cc4ccb98d01b84c27190b90
Vinogradov, Sergei <sergey.vinogradov@intel.com>
2022-10-07 12:00:44 -0400

    Fix moveRegularItemWithSync and add tests

--- cachelib/allocator/tests/AllocatorMemoryTiersTest.h
+++ cachelib/allocator/tests/AllocatorMemoryTiersTest.h
@@ -124,6 +124,98 @@ class AllocatorMemoryTiersTest : public AllocatorTest<AllocatorT> {
     ASSERT(handle != nullptr);
     ASSERT_NO_THROW(alloc->insertOrReplace(handle));
   }
+
+  void testMultiTiersRemoveDuringEviction() {
+    bool quit = false;
+    typename AllocatorT::Config config;
+    config.setCacheSize(4 * Slab::kSize);
+    config.enableCachePersistence("/tmp");
+    config.configureMemoryTiers({
+        MemoryTierCacheConfig::fromShm()
+            .setRatio(1).setMemBind({0}),
+        MemoryTierCacheConfig::fromShm()
+            .setRatio(1).setMemBind({0})
+    });
+
+    std::unique_ptr<AllocatorT> alloc;
+    std::unique_ptr<std::thread> t;
+
+    const auto moveCb = [&] (typename AllocatorT::Item& oldItem,
+                             typename AllocatorT::Item& newItem,
+                             typename AllocatorT::Item* /* parentPtr */) {
+      memcpy(newItem.getMemory(), oldItem.getMemory(), oldItem.getSize());
+      auto key = oldItem.getKey();
+      t = std::make_unique<std::thread>([&](){ alloc->remove(key); });
+      // sleep to make sure async thread calls remove(key)
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      quit = true;
+    };
+    config.enableMovingOnSlabRelease(moveCb, {} /* ChainedItemsMoveSync */,
+                                     -1 /* movingAttemptsLimit */);
+
+    alloc = std::make_unique<AllocatorT>(AllocatorT::SharedMemNew, config);
+    ASSERT(alloc != nullptr);
+    auto pool = alloc->addPool("default", alloc->getCacheMemoryStats().cacheSize);
+
+    int i = 0;
+    while(!quit) {
+      auto handle = alloc->allocate(pool, std::to_string(++i), std::string("value").size());
+      ASSERT(handle != nullptr);
+      ASSERT_NO_THROW(alloc->insertOrReplace(handle));
+    }
+
+    t->join();
+  }
+
+  void testMultiTiersReplaceDuringEviction() {
+    bool quit = false;
+    typename AllocatorT::Config config;
+    config.setCacheSize(4 * Slab::kSize);
+    config.enableCachePersistence("/tmp");
+    config.configureMemoryTiers({
+        MemoryTierCacheConfig::fromShm()
+            .setRatio(1).setMemBind({0}),
+        MemoryTierCacheConfig::fromShm()
+            .setRatio(1).setMemBind({0})
+    });
+
+    std::unique_ptr<AllocatorT> alloc;
+    PoolId pool;
+    std::unique_ptr<std::thread> t;
+
+    const auto moveCb = [&] (typename AllocatorT::Item& oldItem,
+                             typename AllocatorT::Item& newItem,
+                             typename AllocatorT::Item* /* parentPtr */) {
+      memcpy(newItem.getMemory(), oldItem.getMemory(), oldItem.getSize());
+      auto key = oldItem.getKey();
+      if(!quit) {
+        // we need to replace only once because subsequent allocate calls
+        // will cause evictions recursevly
+        quit = true;
+        t = std::make_unique<std::thread>([&](){
+              auto handle = alloc->allocate(pool, key, std::string("new value").size());
+              ASSERT_NO_THROW(alloc->insertOrReplace(handle));
+            });
+        // sleep to make sure async thread calls remove(key)
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      }
+    };
+    config.enableMovingOnSlabRelease(moveCb, {} /* ChainedItemsMoveSync */,
+                                     -1 /* movingAttemptsLimit */);
+
+    alloc = std::make_unique<AllocatorT>(AllocatorT::SharedMemNew, config);
+    ASSERT(alloc != nullptr);
+    pool = alloc->addPool("default", alloc->getCacheMemoryStats().cacheSize);
+
+    int i = 0;
+    while(!quit) {
+      auto handle = alloc->allocate(pool, std::to_string(++i), std::string("value").size());
+      ASSERT(handle != nullptr);
+      ASSERT_NO_THROW(alloc->insertOrReplace(handle));
+    }
+
+    t->join();
+  }
 };
 } // namespace tests
 } // namespace cachelib
