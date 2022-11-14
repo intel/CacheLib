a6eb1e9e911a21f71201cc1fc12ee0bf0fdacca4
Sounak Gupta <sounak.gupta@intel.com>
2022-11-14 02:07:57 -0800

    added ability for compressed pointer to use full 32 bits for addressing in single tier mode and use 31 bits for addressing in multi-tier mode

--- cachelib/allocator/memory/tests/MemoryAllocatorTest.cpp
+++ cachelib/allocator/memory/tests/MemoryAllocatorTest.cpp
@@ -401,13 +401,23 @@ TEST_F(MemoryAllocatorTest, PointerCompression) {
   for (const auto& pool : poolAllocs) {
     const auto& allocs = pool.second;
     for (const auto* alloc : allocs) {
-      CompressedPtr ptr = m.compress(alloc);
+      CompressedPtr ptr = m.compress(alloc, false);
       ASSERT_FALSE(ptr.isNull());
-      ASSERT_EQ(alloc, m.unCompress(ptr));
+      ASSERT_EQ(alloc, m.unCompress(ptr, false));
     }
   }
 
-  ASSERT_EQ(nullptr, m.unCompress(m.compress(nullptr)));
+  ASSERT_EQ(nullptr, m.unCompress(m.compress(nullptr, false), false));
+
+  // test pointer compression with multi-tier
+  for (const auto& pool : poolAllocs) {
+    const auto& allocs = pool.second;
+    for (const auto* alloc : allocs) {
+      MultiTierPtrCompressor ptr = m.compress(alloc, false);
+      ASSERT_FALSE(ptr.isNull());
+      ASSERT_EQ(alloc, m.unCompress(ptr, false));
+    }
+  }
 }
 
 TEST_F(MemoryAllocatorTest, Restorable) {
