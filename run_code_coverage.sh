c6598b7b89365ba84e92793f360bd28e4ae1d073
Sounak Gupta <sounak.gupta@intel.com>
2023-01-04 17:33:03 -0800

    added code coverage support in CacheLib

---
+++ run_code_coverage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BUILD_DIR='build-cachelib'
+mkdir ${BUILD_DIR}
+cd ${BUILD_DIR}
+cmake ../cachelib -DBUILD_TESTS=ON -DCMAKE_INSTALL_PREFIX=/opt -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE_ENABLED=ON
+make install -j
+cd -
+
+# Track coverage
+lcov -c -i -b . -d . -o Coverage.baseline
+./run_tests.sh
+lcov -c -d . -b . -o Coverage.out
+lcov -a Coverage.baseline -a Coverage.out -o Coverage.combined
+
+# Generate report
+COVERAGE_DIR='coverage_report'
+genhtml Coverage.combined -o ${COVERAGE_DIR}
+COVERAGE_REPORT="${COVERAGE_DIR}.tgz"
+tar -zcvf ${COVERAGE_REPORT} ${COVERAGE_DIR}
+echo "Created coverage report ${COVERAGE_REPORT}"
+
+# Cleanup
+rm Coverage.baseline Coverage.out Coverage.combined
+rm -rf ${COVERAGE_DIR}
