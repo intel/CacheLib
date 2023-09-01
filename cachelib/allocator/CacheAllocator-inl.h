d3787b2c027b5221942a46fc679fed3358008dd1
Daniel Byrne <byrnedj12@gmail.com>
2023-09-01 08:09:02 -0700

    fix wakeup bg

--- cachelib/allocator/CacheAllocator-inl.h
+++ cachelib/allocator/CacheAllocator-inl.h
@@ -404,7 +404,11 @@ template <typename CacheTrait>
 bool CacheAllocator<CacheTrait>::shouldWakeupBgEvictor(TierId tid, PoolId pid, ClassId cid) {
   // TODO: should we also work on lower tiers? should we have separate set of params?
   if (tid == 1) return false;
-  return (1-getACStats(tid, pid, cid).usageFraction())*100 <= config_.lowEvictionAcWatermark;
+  double usage = getPoolByTid(pid, tid).getApproxUsage(cid);
+  if (((1-usage)*100) <= config_.lowEvictionAcWatermark) {
+    return true;
+  }
+  return false;
 }
  
 template <typename CacheTrait>
@@ -519,9 +523,9 @@ CacheAllocator<CacheTrait>::allocateInternalTier(TierId tid,
   
   void* memory = allocator_[tid]->allocate(pid, requiredSize);
   
-  //if (backgroundEvictor_.size() && !fromBgThread && (memory == nullptr || shouldWakeupBgEvictor(tid, pid, cid))) {
-  //  backgroundEvictor_[backgroundWorkerId(tid, pid, cid, backgroundEvictor_.size())]->wakeUp();
-  //}
+  if (backgroundEvictor_.size() && !fromBgThread && (memory == nullptr || shouldWakeupBgEvictor(tid, pid, cid))) {
+    backgroundEvictor_[backgroundWorkerId(tid, pid, cid, backgroundEvictor_.size())]->wakeUp();
+  }
   
   if (memory == nullptr && !evict) {
     return {};
